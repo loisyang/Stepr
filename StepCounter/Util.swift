0197e1e8e3d28e312f1c0a95f9c1a3100372ba9f
James Ormond <jormond@andrew.cmu.edu>
2016-09-08 10:32:40 -0400

    Added Util class

---
+++ StepCounter/Util.swift
@@ -0,0 +1,13 @@
+//
+//  Util.swift
+//  StepCounter
+//
+//  Created by James Ormond on 9/8/16.
+//  Copyright © 2016 James Ormond. All rights reserved.
+//
+
+import Foundation
+
+class Util {
+    
+}
