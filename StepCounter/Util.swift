2686d2d0da0f0319cb7c2c35771c1037236bb6a3
James Ormond <jormond@andrew.cmu.edu>
2016-09-08 10:50:06 -0400

    Added Util.initiateDataStructure; set up CoreData for Gadgets

--- StepCounter/Util.swift
+++ StepCounter/Util.swift
@@ -10,4 +10,28 @@ import Foundation
 
 class Util {
     
+    class func initiateDataStructure() {
+        // check if data structure has been established
+        let established = NSUserDefaults.standardUserDefaults().objectForKey("dataStructureInPlace")
+        
+        if established == nil {
+            // data structure has not been established
+            
+            // Step 1: Build all NSUserDefaults variables
+            let currentDate = NSDate()
+            NSUserDefaults.standardUserDefaults().setObject(currentDate, forKey: "downloadDate")
+            NSUserDefaults.standardUserDefaults().setObject(currentDate, forKey: "lastUpdateDate")
+            
+            NSUserDefaults.standardUserDefaults().setValue(0, forKey: "totalPointsSinceStart")
+            NSUserDefaults.standardUserDefaults().setValue(0, forKey: "pointsInWallet")
+            NSUserDefaults.standardUserDefaults().setValue(0, forKey: "daySteps")
+            NSUserDefaults.standardUserDefaults().setValue(0.0, forKey: "dayPoints")
+            
+            // Step 2: Build all gadgets into CoreData
+            
+            // Step 3: Set dataStructureInPlace to true
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "dataStructureInPlace")
+        }
+    }
+    
 }
