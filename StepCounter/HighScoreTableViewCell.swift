d119b2cf250892eabd15908c2c8532082621ba22
James Ormond <jormond@andrew.cmu.edu>
2016-09-10 19:54:23 -0400

    Basic functionality for HighScoreTableViewCell

--- StepCounter/HighScoreTableViewCell.swift
+++ StepCounter/HighScoreTableViewCell.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class HighScoreTableViewCell: UITableViewCell {
+    
+    var dashboard : DashboardModel?
 
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        // Set data to label
+        if let highScoreObject = self.dashboard!.getHighScore() {
+            self.highScoreLabel.text = "High score: \(highScoreObject.points) points"
+        }
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
