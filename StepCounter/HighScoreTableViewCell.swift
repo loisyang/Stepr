bc3d1d00a0eda40e0279c65d201f552e9779d8ed
James Ormond <jormond@andrew.cmu.edu>
2016-09-10 19:35:50 -0400

    Created the 5 prototype cells and corresponding UITableViewCell classes

---
+++ StepCounter/HighScoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HighScoreTableViewCell.swift
+//  Stepr
+//
+//  Created by James Ormond on 9/10/16.
+//  Copyright © 2016 James Ormond. All rights reserved.
+//
+
+import UIKit
+
+class HighScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ StepCounter/TodaysDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodaysDataTableViewCell.swift
+//  Stepr
+//
+//  Created by James Ormond on 9/10/16.
+//  Copyright © 2016 James Ormond. All rights reserved.
+//
+
+import UIKit
+
+class TodaysDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
