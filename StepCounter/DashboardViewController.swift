6b1ff7e6fa89bbe4a0186a53de1fbd752ae13b6a
James Ormond <jormond@andrew.cmu.edu>
2016-09-10 19:30:08 -0400

    Added tableView delegate and data source to DashboardViewController

--- StepCounter/DashboardViewController.swift
+++ StepCounter/DashboardViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let dashboard = DashboardModel()
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Designate this class as the tableView delegate and data source
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
         self.dashboard.pullStepsAndUpdate()
     }
@@ -22,6 +28,15 @@ class DashboardViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
     }
 
 
