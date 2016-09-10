187646010ae75da1c021f637d9faa884b902dfc8
James Ormond <jormond@andrew.cmu.edu>
2016-09-10 19:47:44 -0400

    Set up basic functionality for TodaysDataTableViewCell

--- StepCounter/TodaysDataTableViewCell.swift
+++ StepCounter/TodaysDataTableViewCell.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class TodaysDataTableViewCell: UITableViewCell {
+    
+    var dashboard : DashboardModel?
 
+    @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var stepsLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        // Apply data to labels
+        let steps = self.dashboard!.getStepsForToday()
+        self.stepsLabel.text = "\(steps) steps"
+        
+        let points = self.dashboard!.getPointsForToday()
+        self.pointsLabel.text = "\(points) points"
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
