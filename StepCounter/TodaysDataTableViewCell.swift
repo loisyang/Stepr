9414386a264eb6c84d25c6ed2237ea16722408b7
James Ormond <jormond@andrew.cmu.edu>
2016-09-11 12:08:32 -0400

    Fixed didSet problem for TodaysDataTableViewCell

--- StepCounter/TodaysDataTableViewCell.swift
+++ StepCounter/TodaysDataTableViewCell.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class TodaysDataTableViewCell: UITableViewCell {
     
-    var steps : Int = 0
-    var points : Double = 0
-
+    var steps : Int = 0 {
+        didSet {
+            self.updateLabels()
+        }
+    }
+    var points : Double = 0 {
+        didSet {
+            self.updateLabels()
+        }
+    }
+    
     @IBOutlet weak var pointsLabel: UILabel!
     @IBOutlet weak var stepsLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
-        // Apply data to labels
-        self.stepsLabel.text = "\(self.steps) steps"
-        
-        self.pointsLabel.text = "\(self.points) points"
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    func updateLabels() {
+        self.stepsLabel.text = "\(self.steps) steps"
+        self.pointsLabel.text = "\(self.points) points"
+    }
 }
