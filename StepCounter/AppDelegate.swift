0f7cfa05c7cc3f4d084a5f139a1cc2fa309df486
James Ormond <jormond@andrew.cmu.edu>
2016-09-11 20:22:53 -0400

    Notifications established! Also established background request for HealthKit

--- StepCounter/AppDelegate.swift
+++ StepCounter/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Request permission from user to recieve notifications
+        // types are UIUserNotificationType values
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil))
         return true
     }
 
