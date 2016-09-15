2e17016d7037d63bf29a214619e874d819bf5b50
James Ormond <jormond@andrew.cmu.edu>
2016-09-14 22:28:03 -0400

    Created files GadgetStoreViewController and GadgetDescriptionViewController

---
+++ StepCounter/GadgetDescriptionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GadgetDescriptionViewController.swift
+//  Stepr
+//
+//  Created by James Ormond on 9/14/16.
+//  Copyright © 2016 James Ormond. All rights reserved.
+//
+
+import UIKit
+
+class GadgetDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ StepCounter/GadgetStoreViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GadgetStoreViewController.swift
+//  Stepr
+//
+//  Created by James Ormond on 9/14/16.
+//  Copyright © 2016 James Ormond. All rights reserved.
+//
+
+import UIKit
+
+class GadgetStoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
