6b4786797f4c33ed9584ea780bb61a29f832d58b
James Ormond <jormond@andrew.cmu.edu>
2016-09-15 20:38:06 -0400

    Added GadgetModel starter code

--- StepCounter/GadgetStoreViewController.swift
+++ StepCounter/GadgetStoreViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class GadgetStoreViewController: UIViewController {
+class GadgetStoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //let gadgetsModel = GadgetsModel()
+    let gadgetList = ["test", "test2", "test3"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Designate this class as the tableView delegate and data source
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateTableData() {
+        self.tableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return self.gadgetModel.numberOfGadgetsAvailable()
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // let cell = tableView.dequeueReusableCellWithIdentifier(gadgetList[indexPath.row])
+        let cell = UITableViewCell()
+        cell.textLabel?.text = gadgetList[indexPath.row]
+        return cell
     }
     
     
