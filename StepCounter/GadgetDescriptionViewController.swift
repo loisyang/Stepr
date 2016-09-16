1ad13867c21710b41303299ef1d59d55f3b0bea0
James Ormond <jormond@andrew.cmu.edu>
2016-09-16 10:50:56 -0400

    Purchase functionality working for Gadgets

--- StepCounter/GadgetDescriptionViewController.swift
+++ StepCounter/GadgetDescriptionViewController.swift
@@ -11,26 +11,44 @@ import UIKit
 class GadgetDescriptionViewController: UIViewController {
     
     var gadget : Gadget?
-
+    
     @IBOutlet weak var gadgetNameLabel: UILabel!
     @IBOutlet weak var gadgetCostLabel: UILabel!
     @IBOutlet weak var gadgetCountLabel: UILabel!
+    @IBOutlet weak var purchaseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
-        if let gadget = self.gadget {
-            self.gadgetNameLabel.text = gadget.name!
-            self.gadgetCostLabel.text = "\(gadget.cost as! Int) points"
-            self.gadgetCountLabel.text = "\(gadget.numActive as! Int) active"
-        }
+        self.updateLabels()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateLabels() {
+        if let gadget = self.gadget {
+            self.gadgetNameLabel.text = gadget.name!
+            self.gadgetCostLabel.text = "\(gadget.cost as! Int) points"
+            self.gadgetCountLabel.text = "\(gadget.numActive as! Int) active"
+            
+            if !gadget.canPurchase() {
+                self.purchaseButton.hidden = true
+            }
+        }
+        
+    }
+    
+    @IBAction func purchasePressed(sender: AnyObject) {
+        if let gadget = self.gadget {
+            if gadget.canPurchase() {
+                gadget.addOne()
+                self.updateLabels()
+            }
+        }
+    }
 }
