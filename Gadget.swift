ee335ffc5b4e5cacdf253edeb8fca77ab3c6f837
James Ormond <jormond@andrew.cmu.edu>
2016-09-16 10:45:57 -0400

    Completed Gadget.swift functions, added canPurchase()

--- Gadget.swift
+++ Gadget.swift
@@ -10,32 +10,48 @@ import Foundation
 import CoreData
 import UIKit
 class Gadget: NSManagedObject {
+    
+    func canPurchase() -> Bool {
+        let cost = self.cost as! Double
+        let walletPoints = NSUserDefaults.standardUserDefaults().valueForKey("pointsInWallet") as! Double
+        return walletPoints >= cost
+    }
 
 // Insert code here to add functionality to your managed object subclass
     
     func addOne(){
         
-        //update coredata
-        let app = (UIApplication.sharedApplication().delegate as! AppDelegate)
-        let context = app.managedObjectContext
-        var fetchRequest = NSFetchRequest(entityName: "Gadget")
-        fetchRequest.predicate = NSPredicate(format: "name = %@", self.name!)
-        do {
-            let gadgets = try context.executeFetchRequest(fetchRequest) as! [Gadget]
-            if gadgets.count != 0 {
-                var gadget = gadgets[0]
-                //increase numActive by one
-                gadget.setValue(Int(gadget.numActive!) + 1 ,forKey: "numActive")
-                try context.save()
-                
-                //deduct cost from total points
-                let walletPoints = NSUserDefaults.standardUserDefaults().valueForKey("pointsInWallet") as! Double
-                let newWalletTotal = walletPoints - Double(gadget.cost!)
-                NSUserDefaults.standardUserDefaults().setValue(newWalletTotal, forKey: "pointsInWallet")
+        if self.canPurchase() {
+            
+            //update coredata
+            let app = (UIApplication.sharedApplication().delegate as! AppDelegate)
+            let context = app.managedObjectContext
+            let fetchRequest = NSFetchRequest(entityName: "Gadget")
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.name!)
+            do {
+                let gadgets = try context.executeFetchRequest(fetchRequest) as! [Gadget]
+                if gadgets.count != 0 {
+                    let gadget = gadgets[0]
+                    
+                    //deduct cost from total points
+                    let walletPoints = NSUserDefaults.standardUserDefaults().valueForKey("pointsInWallet") as! Double
+                    let newWalletTotal = walletPoints - Double(gadget.cost!)
+                    NSUserDefaults.standardUserDefaults().setValue(newWalletTotal, forKey: "pointsInWallet")
+                    
+                    // modify Gadget
+                    // increase numActive by one
+                    gadget.setValue(Int(gadget.numActive!) + 1 ,forKey: "numActive")
+                    // increase cost by 1.2x
+                    gadget.setValue(Int(floor((gadget.cost as! Double) * 1.2)), forKey:  "cost")
+                    
+                    try context.save()
+                }
+            } catch {
+                fatalError("Failed to fetch gadgets: \(error)")
             }
-        } catch {
-            fatalError("Failed to fetch gadgets: \(error)")
+
         }
+        
     }
     
     class func calculatePoints(newSteps: Int) -> Double {
@@ -56,7 +72,7 @@ class Gadget: NSManagedObject {
             var bonus:Double = 1
             for gadget in gadgets {
                 if (Int(gadget.numActive!) > 0){
-                    bonus += Double(gadget.bonus!)
+                    bonus += Double((gadget.bonus as! Double) * (gadget.numActive as! Double))
                 }
                 
             }
