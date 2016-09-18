c99c4549a12cfccdb79b297022999ca38139ac7a
James Ormond <jormond@andrew.cmu.edu>
2016-09-17 22:29:58 -0400

    Set up achievements view controller

---
+++ StepCounter/AchievemntsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AchievemntsViewController.swift
+//  Stepr
+//
+//  Created by James Ormond on 9/17/16.
+//  Copyright © 2016 James Ormond. All rights reserved.
+//
+
+import UIKit
+
+class AchievemntsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
