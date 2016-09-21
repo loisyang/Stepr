d5b364b6434619e7c1cca5fa927c3d2514e5a7d8
James Ormond <jormond@andrew.cmu.edu>
2016-09-21 13:33:01 -0400

    Set up animation for Level Bar

--- StepCounter/LevelTableViewCell.swift
+++ StepCounter/LevelTableViewCell.swift
@@ -18,11 +18,17 @@ class LevelTableViewCell: UITableViewCell {
     
     var percentage : Double = 0 {
         didSet {
-            self.updateLabels()
+            self.animatePercentage()
         }
     }
+    
+    var timer : NSTimer = NSTimer()
 
-    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var levelProgressBar: UIView!
+    
+    let bar = UIView()
+    
+    var count : Double = 0
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -36,7 +42,27 @@ class LevelTableViewCell: UITableViewCell {
     }
     
     func updateLabels() {
-        self.levelLabel.text = "Level \(self.level)"
+        //self.levelLabel.text = "Level \(self.level)"
+    }
+    
+    func animatePercentage() {
+        let view = self.levelProgressBar
+        self.bar.frame = CGRect(x: 0, y: 0, width: 0, height: view.frame.size.height)
+        self.bar.backgroundColor = UIColor.redColor()
+        view.addSubview(bar)
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.001, target: self, selector:#selector(LevelTableViewCell.setProgress), userInfo: nil, repeats: true)
+    }
+    
+    func setProgress() {
+        if count >= 1 {
+            self.timer.invalidate()
+        } else {
+            let portion = 0.001 * (CGFloat(self.percentage) * self.levelProgressBar.frame.size.width)
+            self.bar.frame.size.width += portion
+            print(self.bar.frame.size.width)
+            print(self.bar.frame.size.width / self.levelProgressBar.frame.size.width)
+            self.count += 0.001
+        }
     }
 
 }
