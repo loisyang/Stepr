5190ed36730319e32bc41511dc352fdca5861118
James Ormond <jormond@andrew.cmu.edu>
2016-09-21 20:56:28 -0400

    Implemented the new level structure

--- StepCounter/DashboardModel.swift
+++ StepCounter/DashboardModel.swift
@@ -109,47 +109,47 @@ class DashboardModel {
      Below is a structure of the level ranges:
      
      Level 1: 0 - 5000
-     Level 2: 5000 - 20000
-     Level 3: 20000 - 50000
-     Level 4: 50000 - 100000
-     Level 5: 100000 - 200000
-     Level 6: 200000 - 500000
-     Level 7: 500000 - 1 mil
-     Level 8: 1 mil - 2 mil
-     Level 9: 2 mil - 5 mil
-     Level 10: 5 mil - 10 mil
-     Level 11: 10 mil - 20 mil
-     Level 12: 20 mil - 50 mil
-     Level 13: 50 mil - 100 mil
-     Level 14: 100 mil - 500 mil
-     Level 15: 500 mil - 1 bil
-     Level 16: 1 bil - 10 bil
-     Level 17: 10 bil - 100 bil
-     Level 18: 100 bil - 1 tril
-     Level 19: 1 tril - 1 quad
-     Level 20: 1 quad -
+     Level 2: 5000 - 15000
+     Level 3: 15000 - 38000
+     Level 4: 38000 - 98835
+     Level 5: 98835 - 283879
+     Level 6: 283879 - 931168
+     Level 7: 931168 - 3.535 million
+     Level 8: 3.535 million - 15.581 million
+     Level 9: 15.581 million - 79.665 million
+     Level 10: 79.665 million - 471.734 million
+     Level 11: 471.734 million - 3.23 billion
+     Level 12: 3.23 billion - 25.55 billion
+     Level 13: 25.55 billion - 233.226 billion
+     Level 14: 233.226 billion - 2.455 trillion
+     Level 15: 2.455 trillion - 29.801 trillion
+     Level 16: 29.801 trillion - 416.786 trillion
+     Level 17: 416.786 trillion - 6.715 quadrillion
+     Level 18: 6.715 quadrillion - 124.58 quadrillion
+     Level 19: 124.58 quadrillion - 2.661 quintillion
+     Level 20: 2.661 quintillion -
      */
     func getUserLevel() -> (level: Int, percentage: Double, pointsToGo: Double) {
         let pointsRanges : [(min: Double, max: Double)] = [
             (0, 5000),
-            (5000, 20000),
-            (20000, 50000),
-            (50000, 100000),
-            (100000, 200000),
-            (200000, 500000),
-            (500000, 1000000), // 1 mil
-            (1000000, 2000000), // 2 mil
-            (2000000, 5000000), // 5 mil
-            (5000000, 10000000), // 10 mil
-            (10000000, 20000000), // 20 mil
-            (20000000, 50000000), // 50 mil
-            (50000000, 100000000), // 100 mil
-            (100000000, 500000000), // 500 mil
-            (500000000, 1000000000), // 1 bil
-            (1000000000, 10000000000), // 10 bil
-            (10000000000, 100000000000), // 100 bil
-            (100000000000, 1000000000000), // 1 tril
-            (1000000000000, 1000000000000000), // 1 quad
+            (5000, 15000),
+            (15000, 38000),
+            (38000, 98835),
+            (98835, 283879),
+            (283879, 931168),
+            (931168, 3535000), // 931168 - 3.535 million
+            (3535000, 15581000), // 3.535 million - 15.581 million
+            (15581000, 79665000), // 15.581 million - 79.665 million
+            (79665000, 471734000), // 79.665 million - 471.734 million
+            (471734000, 3230000000), // 471.734 million - 3.23 billion
+            (3230000000, 25550000000), // 3.23 billion - 25.55 billion
+            (25550000000, 233226000000), // 25.55 billion - 233.226 billion
+            (233226000000, 2455000000000), // 233.226 billion - 2.455 trillion
+            (2455000000000, 29801000000000), // 2.455 trillion - 29.801 trillion
+            (29801000000000, 416786000000000), // 29.801 trillion - 416.786 trillion
+            (416786000000000, 6715000000000000), // 416.786 trillion - 6.715 quadrillion
+            (6715000000000000, 124580000000000000), // 6.715 quadrillion - 124.58 quadrillion
+            (124580000000000000, 2661000000000000000), // 124.58 quadrillion - 2.661 quintillion
         ]
         
         if let totalPoints = NSUserDefaults.standardUserDefaults().valueForKey("totalPointsSinceStart") {
